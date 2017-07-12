cac979932f472e0d2d409f03ee6491cd63721d2f
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-07-12 23:11:33 +0530

    Changing User(model class) from Java to Kotlin

---
+++ app/src/main/java/com/example/ravneet/ieeedtu/infrasturcture/User.kt
@@ -0,0 +1,13 @@
+package com.example.ravneet.ieeedtu.infrasturcture
+
+/**
+ * Created by ravneet on 12/7/17.
+ */
+data class User(
+        val username : String,
+        val emailId : String,
+        val mobileNumber : String,
+        val Post : String,
+        val password : String,
+        val year : String
+)
