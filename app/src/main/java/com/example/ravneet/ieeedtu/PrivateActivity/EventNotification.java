ef839fc25bd4ca7e4cec6e37686824940e52a900
Ravneet Punia <ravneet.dtu@Gmail.com>
2017-07-18 22:29:01 +0530

    Creating Sample UI For Presentation Of APP

--- app/src/main/java/com/example/ravneet/ieeedtu/PrivateActivity/EventNotification.java
+++ app/src/main/java/com/example/ravneet/ieeedtu/PrivateActivity/EventNotification.java
@@ -1,15 +1,28 @@
 package com.example.ravneet.ieeedtu.PrivateActivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ravneet.ieeedtu.R;
 
 public class EventNotification extends AppCompatActivity {
 
+    Button btn_postSigNotification;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_notification);
+
+        btn_postSigNotification = (Button) findViewById(R.id.btn_PostSigNotification);
+        btn_postSigNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EventNotification.this,SIGInformation.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/ravneet/ieeedtu/infrasturcture/SIG.kt
@@ -0,0 +1,11 @@
+package com.example.ravneet.ieeedtu.infrasturcture
+
+/**
+ * Created by ravneet on 17/7/17.
+ */
+data class SIG(
+        val title : String,
+        val body : String,
+        val date : String,
+        val place : String
+)
